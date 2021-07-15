7b175effccadc6b7975cbed357a33f8400a1a2f9
ImmuniWeb <ssl@immuniweb.com>
2021-07-16 00:06:14 +0300

    Logging level Typo

--- iwtools/lib/logger.py
+++ iwtools/lib/logger.py
@@ -20,7 +20,7 @@ def init_logging(file_path, output_format):
 
     # Disable logging if raw results
     if output_format in ['raw_json', 'pretty_json']:
-        logging.disable(level=CRITICAL)
+        logging.disable(level=logging.CRITICAL)
 
     # Init logging
     logging.basicConfig(

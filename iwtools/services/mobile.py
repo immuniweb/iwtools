fc47725e55f81cebab2639b228b2094100ded212
ImmuniWeb <ssl@immuniweb.com>
2021-10-28 05:08:40 +0300

    Better mobile service error handling

--- iwtools/services/mobile.py
+++ iwtools/services/mobile.py
@@ -1,19 +1,15 @@
 #! /usr/bin/env python3
 
-# Standard libraries
-from time import time, strftime, localtime, sleep
-from urllib.parse import urlparse
-import logging
 import asyncio
-import sys
+import logging
 import re
+import sys
+from time import localtime, sleep, strftime, time
+from urllib.parse import urlparse
 
-# Third-party libraries
-from termcolor import colored
 import requests
-
-# Local libraries
 from gui.loader import InfiniteLoader
+from termcolor import colored
 
 
 class Mobile:
@@ -26,9 +22,8 @@ class Mobile:
     ✓ Static & Dynamic Mobile Sca
     """
 
-    API_URL = 'https://www.immuniweb.com/mobile/api'
-    USER_AGENT = 'iwtools-0.1'
-
+    API_URL = "https://www.immuniweb.com/mobile/api"
+    USER_AGENT = "iwtools-0.1"
 
     def __init__(self, target, api_key=None, recheck=False, quiet=False):
         self.target = target
@@ -37,11 +32,9 @@ class Mobile:
         self.quiet = quiet
         self.test_results = None
         self.session = requests.Session()
-        self.session.headers.update({
-            'User-Agent': self.USER_AGENT,
-            'Connection':'close'
-        })
-
+        self.session.headers.update(
+            {"User-Agent": self.USER_AGENT, "Connection": "close"}
+        )
 
     def get_cache_id_or_start_test(self):
         """Get cached test id if exists or start new test"""
@@ -49,15 +42,12 @@ class Mobile:
         url = f"{self.API_URL}/download_apk"
 
         files = None
-        data = {
-            'api_key': self.api_key,
-            'hide_in_statistics': 1
-        }
+        data = {"api_key": self.api_key, "hide_in_statistics": 1}
 
-        if urlparse(self.target).scheme in ['http', 'https']:
-            data['app'] = self.target
+        if urlparse(self.target).scheme in ["http", "https"]:
+            data["app"] = self.target
         else:
-            files = {'file': open(self.target,'rb')}
+            files = {"file": open(self.target, "rb")}
             url = f"{self.API_URL}/upload"
 
         response = self.session.post(url, data=data, files=files)
@@ -66,7 +56,6 @@ class Mobile:
         logging.debug(response.text)
 
         return response.json()
-
 
     def get_result_by_test_id(self, test_id):
         """Get result by test id (from cache)"""
@@ -80,16 +69,12 @@ class Mobile:
 
         return response.json()
 
-
     def get_result_by_job_id(self, job_id):
         """Get result by job id (processing)"""
 
         url = f"{self.API_URL}/get_result/{str(time())}.html"
 
-        data = {
-            'job_id': job_id,
-            'verbosity': 1
-        }
+        data = {"job_id": job_id, "verbosity": 1}
 
         response = self.session.post(url, data)
         response.raise_for_status()
@@ -97,7 +82,6 @@ class Mobile:
         logging.debug(response.text)
 
         return response.json()
-
 
     async def test_loader(self, start_message, cancel_message):
         """Print loading status while test rinning"""
@@ -108,35 +92,47 @@ class Mobile:
         row_clear = "\x1B[0K"
         step = 0
 
-        logging.info(colored(start_message, 'blue'))
+        logging.info(colored(start_message, "blue"))
 
         # Draw loader
-        print('\n\n\n')
+        print("\n\n\n")
         while True:
             print(row_up + loader.generate(step) + row_clear)
-            print('\n' + row_clear)
+            print("\n" + row_clear)
 
             step += 1
             try:
-                await asyncio.sleep(.1)
+                await asyncio.sleep(0.1)
             except asyncio.CancelledError:
-                logging.info(colored(row_up + row_clear + row_up + row_clear + '\n' + row_clear + cancel_message + '\n' , 'blue'))
+                logging.info(
+                    colored(
+                        row_up
+                        + row_clear
+                        + row_up
+                        + row_clear
+                        + "\n"
+                        + row_clear
+                        + cancel_message
+                        + "\n",
+                        "blue",
+                    )
+                )
                 break
-
 
     def wait_test_results(self, test_id):
         """Wait until the completion of test"""
 
-        response = {
-            'status': 'unprocessed'
-        }
+        response = {"status": "unprocessed"}
 
-        while 'status' in response and response['status'] in ['in_progress', 'unprocessed', 'doesnt_exist']:
+        while "status" in response and response["status"] in [
+            "in_progress",
+            "unprocessed",
+            "doesnt_exist",
+        ]:
             response = self.get_result_by_test_id(test_id)
             sleep(10)
 
         return response
-
 
     async def watch(self, test_id):
         """Check test status until complete"""
@@ -145,7 +141,11 @@ class Mobile:
             response = self.wait_test_results(test_id)
         else:
             loop = asyncio.get_event_loop()
-            loader = asyncio.ensure_future(self.test_loader('Mobile App Security Test in progress', 'Test completed'))
+            loader = asyncio.ensure_future(
+                self.test_loader(
+                    "Mobile App Security Test in progress", "Test completed"
+                )
+            )
             response = await loop.run_in_executor(None, self.wait_test_results, test_id)
             loader.cancel()
 
@@ -153,34 +153,34 @@ class Mobile:
 
         return response
 
-
     def start(self):
         """Start test"""
 
         logging.info(
-            colored('\nImmuni', 'red', attrs=['bold']) + colored('Web®', 'blue', attrs=['bold']) +
-            colored(' Community Edition: Mobile App Security Test\n', attrs=['bold'])
+            colored("\nImmuni", "red", attrs=["bold"])
+            + colored("Web®", "blue", attrs=["bold"])
+            + colored(" Community Edition: Mobile App Security Test\n", attrs=["bold"])
         )
 
-        logging.info(colored('Target: ', attrs=['bold']) + self.target + '\n')
+        logging.info(colored("Target: ", attrs=["bold"]) + self.target + "\n")
 
         response = self.get_cache_id_or_start_test()
 
         error = None
         for task in response:
-            if 'status' and 'id' in task and task['status'] == 'success':
+            if "status" and "id" in task and task["status"] == "success":
                 loop = asyncio.get_event_loop()
-                response = loop.run_until_complete(self.watch(task['id']))
+                response = loop.run_until_complete(self.watch(task["id"]))
                 loop.close()
                 error = None
                 break
 
-            elif 'error_id' in task and task['error_id'] == 0:
-                response = self.get_result_by_test_id(task['id'])
+            elif "error_id" in task and task["error_id"] == 0:
+                response = self.get_result_by_test_id(task["id"])
 
-                if response['status'] in ['in_progress', 'unprocessed', 'doesnt_exist']:
+                if response["status"] in ["in_progress", "unprocessed", "doesnt_exist"]:
                     loop = asyncio.get_event_loop()
-                    response = loop.run_until_complete(self.watch(task['id']))
+                    response = loop.run_until_complete(self.watch(task["id"]))
                     loop.close()
                     error = None
                     break
@@ -188,25 +188,36 @@ class Mobile:
                 error = None
                 break
 
-            if 'error' in task:
-                error = task['error']
+            if "error" in task:
+                error = task["error"]
+                break
+
+            if "status" in task and task["status"] == "error":
+                error = task["message"]
+                break
 
         if error:
-            raise Exception(error)
+            raise Exception(self.normalize_text(error))
 
         self.test_results = response
 
         return response
 
-
     def normalize_color(self, color):
         """Adapt color names to console palette"""
 
-        if color == 'orange':
-            return 'yellow'
+        if color == "orange":
+            return "yellow"
 
         return color
 
+    def normalize_text(self, text):
+        """Fix text special chars"""
+
+        text = text.replace("&#039;", "'")
+        text = text.replace("&quot;", '"')
+
+        return text
 
     def generate_banner(self, owasp_color, behaviour_color, sca_color, apis_color):
         """Generate beauty banner with test results"""
@@ -221,39 +232,86 @@ class Mobile:
             f"{colored('╰──────────────────────────────────╯', sca_color)} {colored('╰──────────────────────────────────────╯', behaviour_color)}\n"
         )
 
-
     def print_report(self, test_results=None):
         """Generate report"""
 
         if not test_results:
             test_results = self.test_results
 
-        owasp_color = self.normalize_color(test_results['scores']['owasp_top_10']['class'])
-        behaviour_color = self.normalize_color(test_results['scores']['behaviour']['class'])
-        sca_color = self.normalize_color(test_results['scores']['sca']['class'])
-        apis_color = self.normalize_color(test_results['scores']['apis']['class'])
+        owasp_color = self.normalize_color(
+            test_results["scores"]["owasp_top_10"]["class"]
+        )
+        behaviour_color = self.normalize_color(
+            test_results["scores"]["behaviour"]["class"]
+        )
+        sca_color = self.normalize_color(test_results["scores"]["sca"]["class"])
+        apis_color = self.normalize_color(test_results["scores"]["apis"]["class"])
 
-        banner = self.generate_banner(owasp_color, behaviour_color, sca_color, apis_color)
+        banner = self.generate_banner(
+            owasp_color, behaviour_color, sca_color, apis_color
+        )
 
-        test_time = strftime('%B %d, %Y %H:%M:%S', localtime(test_results['data']['app_info']['ts_stop']))
+        test_time = strftime(
+            "%B %d, %Y %H:%M:%S", localtime(test_results["data"]["app_info"]["ts_stop"])
+        )
 
         # Test is outdated if older than 1 week
         test_time_color = None
-        if int(time()) - test_results['data']['app_info']['ts_stop'] > 604800:
-            test_time_color = 'yellow'
+        if int(time()) - test_results["data"]["app_info"]["ts_stop"] > 604800:
+            test_time_color = "yellow"
 
-        logging.info(colored("App Name: ", attrs=['bold']) + test_results['data']['app_info']['app_name'])
-        logging.info(colored("App ID: ", attrs=['bold']) + test_results['data']['app_info']['app_id'])
-        logging.info(colored("Version: ", attrs=['bold']) + test_results['data']['app_info']['app_version'])
-        logging.info(colored("Developer: ", attrs=['bold']) + test_results['data']['app_info']['app_developer'])
-        logging.info(colored("OS: ", attrs=['bold']) + test_results['data']['app_info']['device_type'].title())
-        logging.info(colored("Completed: ", attrs=['bold']) + colored(test_time, test_time_color))
+        logging.info(
+            colored("App Name: ", attrs=["bold"])
+            + test_results["data"]["app_info"]["app_name"]
+        )
+        logging.info(
+            colored("App ID: ", attrs=["bold"])
+            + test_results["data"]["app_info"]["app_id"]
+        )
+        logging.info(
+            colored("Version: ", attrs=["bold"])
+            + test_results["data"]["app_info"]["app_version"]
+        )
+        logging.info(
+            colored("Developer: ", attrs=["bold"])
+            + test_results["data"]["app_info"]["app_developer"]
+        )
+        logging.info(
+            colored("OS: ", attrs=["bold"])
+            + test_results["data"]["app_info"]["device_type"].title()
+        )
+        logging.info(
+            colored("Completed: ", attrs=["bold"]) + colored(test_time, test_time_color)
+        )
         logging.info(banner)
-        logging.info(colored("OWASP Mobile Top 10 Security Test: ", attrs=['bold'])  + colored(test_results['scores']['owasp_top_10']['description'].title(), owasp_color))
-        logging.info(colored("Mobile App Privacy and Behavior: ", attrs=['bold'])  + colored(test_results['scores']['behaviour']['description'].title(), behaviour_color))
-        logging.info(colored("Software Composition Analysis Test: ", attrs=['bold'])  + colored(test_results['scores']['sca']['description'].title(), sca_color))
-        logging.info(colored("Mobile App External Communications: ", attrs=['bold'])  + colored(test_results['scores']['apis']['description'].title(), apis_color))
-
+        logging.info(
+            colored("OWASP Mobile Top 10 Security Test: ", attrs=["bold"])
+            + colored(
+                test_results["scores"]["owasp_top_10"]["description"].title(),
+                owasp_color,
+            )
+        )
+        logging.info(
+            colored("Mobile App Privacy and Behavior: ", attrs=["bold"])
+            + colored(
+                test_results["scores"]["behaviour"]["description"].title(),
+                behaviour_color,
+            )
+        )
+        logging.info(
+            colored("Software Composition Analysis Test: ", attrs=["bold"])
+            + colored(test_results["scores"]["sca"]["description"].title(), sca_color)
+        )
+        logging.info(
+            colored("Mobile App External Communications: ", attrs=["bold"])
+            + colored(test_results["scores"]["apis"]["description"].title(), apis_color)
+        )
 
         # Full Results
-        logging.info(colored("\nCheck Details: ", attrs=['bold']) + colored(f"https://www.immuniweb.com/mobile/{test_results['data']['app_info']['app_id']}/{test_results['data']['app_info']['test_short_id']}/", 'blue'))
+        logging.info(
+            colored("\nCheck Details: ", attrs=["bold"])
+            + colored(
+                f"https://www.immuniweb.com/mobile/{test_results['data']['app_info']['app_id']}/{test_results['data']['app_info']['test_short_id']}/",
+                "blue",
+            )
+        )
